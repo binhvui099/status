d2a7de58a55fe7fdf09583c40af974716cba259f
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 12:54:45 +0700

    Add files via upload

---
+++ checkvpsos.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# check os
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/checkos.sh) && sleep 2 && exit 1
+fi
+
+clear
+echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e ""
+echo "          ███████████   ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████       ██          ██   ██          ██   ██████████"
+echo "          ██       ██   ██       ██   ██            ██            ██       ██   ██       ██   ██       ██        ██        ██     ██        ██            ██"
+echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                  ██      ██       ██      ██            ██"
+echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                   ██    ██         ██    ██            ██"
+echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                    ██  ██           ██  ██            ██"
+echo "          ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████   ██                     ████             ████            ██"
+echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██████            ████              ██            ██"
+echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██  ██           ██  ██             ██           ██"
+echo "                   ██   ██            ██            ██            ██       ██            ██   ██       ██          ██    ██            ██          ██"
+echo "          ██       ██   ██            ██            ██            ██       ██            ██   ██       ██         ██      ██           ██         ██"
+echo "          ███████████   ██            ███████████   ███████████   ███████████            ██   ███████████   ██   ██        ██          ██         ██████████"
+echo -e ""
+echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e ""
+echo "                                                      Quá Trình Cài Đặt XrayR Đang Diễn Ra !"
+bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timesetupxrayr.sh)
