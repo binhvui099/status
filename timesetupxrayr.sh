a8a3456796336860a5ee57eca40ee6f287670edc
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 12:22:09 +0700

    Update timesetupxrayr.sh

--- timesetupxrayr.sh
+++ timesetupxrayr.sh
@@ -18,3 +18,4 @@ echo "          ----------------------------------------------------------------
 echo -e ""
 echo "                                     Vui Lòng Chờ 5 Giây Để Bắt Đầu Tiến Hành Quá Trình Cài Đặt XrayR Cho Máy Chủ Của Bạn !"
 sleep 5
+bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/startinstallxrayr.sh)
