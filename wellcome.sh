a88604b784ca02abeef66c21254e8a98d60d2404
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 12:16:04 +0700

    Update wellcome.sh

--- wellcome.sh
+++ wellcome.sh
@@ -17,5 +17,5 @@ echo -e ""
 echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
 echo "                                                           Chào Mừng Bạn Đên Với XrayR 1 Stack !"
-sleep 3
+sleep 5
 bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/copyright.sh)
