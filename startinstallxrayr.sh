414d1773ef6671d0b072c93d73a54709bbcb68a5
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 12:51:48 +0700

    Update startinstallxrayr.sh

--- startinstallxrayr.sh
+++ startinstallxrayr.sh
@@ -18,3 +18,5 @@ echo "          ----------------------------------------------------------------
 echo -e ""
 echo "                                                     Bắt Đầu Quá Trình Cài Đặt XrayR Cho Máy Chủ Của Bạn"
 echo -e ""
+sleep 2
+bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/checkvpsroot.sh)
