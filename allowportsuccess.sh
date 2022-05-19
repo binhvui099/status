d7f9dc541ebe3521658bcfb62e96382e132c4253
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 12:17:27 +0700

    Update allowportsuccess.sh

--- allowportsuccess.sh
+++ allowportsuccess.sh
@@ -17,5 +17,5 @@ echo -e ""
 echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
 echo "                                                Thông Báo: Mở Port 80 Và Port 443 Thành Công !"
-sleep 3
+sleep 5
 bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timesetupxrayr.sh)
