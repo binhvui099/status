c30e59ce9d43843f35d6d0846e8bdd5976799ba5
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 13:49:31 +0700

    Update startinstallxrayr.sh

--- startinstallxrayr.sh
+++ startinstallxrayr.sh
@@ -19,4 +19,3 @@ echo -e ""
 echo "                                                     Bắt Đầu Quá Trình Cài Đặt XrayR Cho Máy Chủ Của Bạn"
 echo -e ""
 sleep 2
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/installsetup.sh)
