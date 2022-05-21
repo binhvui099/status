41ea157edbf1fbd2f844e27e75c5f32875fc2227
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-21 20:49:35 +0700

    Update luachonv2board.sh

--- luachonv2board.sh
+++ luachonv2board.sh
@@ -83,61 +83,61 @@ echo -e ""
     echo && read -p "  Vui Lòng Nhập Một Lựa Chọn [1-28]: " num
 
     case "${num}" in
-        1) git clone https://github.com/source235/v2board155.git ./ && unzip -u V2B155.zip && rm -rf V2B155.zip && clear && sh init.sh
+        1) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board155.git ./ && unzip -u V2B155.zip && rm -rf V2B155.zip && clear && sh init.sh
         ;;
-        2) git clone https://github.com/source235/v2board154.git ./ && unzip -u V2B154.zip && rm -rf V2B154.zip && clear && sh init.sh
+        2) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board154.git ./ && unzip -u V2B154.zip && rm -rf V2B154.zip && clear && sh init.sh
         ;;
-        3) git clone https://github.com/source235/v2board153.git ./ && unzip -u V2B153.zip && rm -rf V2B153.zip && clear && sh init.sh
+        3) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board153.git ./ && unzip -u V2B153.zip && rm -rf V2B153.zip && clear && sh init.sh
         ;;
-        4) git clone https://github.com/source235/v2board152.git ./ && unzip -u V2B152.zip && rm -rf V2B152.zip && clear && sh init.sh
+        4) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board152.git ./ && unzip -u V2B152.zip && rm -rf V2B152.zip && clear && sh init.sh
         ;;
-        5) git clone https://github.com/source235/v2board151.git ./ && unzip -u V2B151.zip && rm -rf V2B151.zip && clear && sh init.sh
+        5) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board151.git ./ && unzip -u V2B151.zip && rm -rf V2B151.zip && clear && sh init.sh
         ;;
-        6) git clone https://github.com/source235/v2board150.git ./ && unzip -u V2B150.zip && rm -rf V2B150.zip && clear && sh init.sh
+        6) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board150.git ./ && unzip -u V2B150.zip && rm -rf V2B150.zip && clear && sh init.sh
         ;;
-        7) git clone https://github.com/source235/v2board143.git ./ && unzip -u V2B143.zip && rm -rf V2B143.zip && clear && sh init.sh
+        7) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board143.git ./ && unzip -u V2B143.zip && rm -rf V2B143.zip && clear && sh init.sh
         ;;
-        8) git clone https://github.com/source235/v2board142.git ./ && unzip -u V2B142.zip && rm -rf V2B142.zip && clear && sh init.sh
+        8) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board142.git ./ && unzip -u V2B142.zip && rm -rf V2B142.zip && clear && sh init.sh
         ;;
-        9) git clone https://github.com/source235/v2board141.git ./ && unzip -u V2B141.zip && rm -rf V2B141.zip && clear && sh init.sh
+        9) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board141.git ./ && unzip -u V2B141.zip && rm -rf V2B141.zip && clear && sh init.sh
         ;;
-        10) git clone https://github.com/source235/v2board140.git ./ && unzip -u V2B140.zip && rm -rf V2B140.zip && clear && sh init.sh
+        10) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board140.git ./ && unzip -u V2B140.zip && rm -rf V2B140.zip && clear && sh init.sh
         ;;
-        11) git clone https://github.com/source235/v2board132.git ./ && unzip -u V2B132.zip && rm -rf V2B132.zip && clear && sh init.sh
+        11) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board132.git ./ && unzip -u V2B132.zip && rm -rf V2B132.zip && clear && sh init.sh
         ;;
-        12) git clone https://github.com/source235/v2board131.git ./ && unzip -u V2B131.zip && rm -rf V2B131.zip && clear && sh init.sh
+        12) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board131.git ./ && unzip -u V2B131.zip && rm -rf V2B131.zip && clear && sh init.sh
         ;;
-        13) git clone https://github.com/source235/v2board130.git ./ && unzip -u V2B130.zip && rm -rf V2B130.zip && clear && sh init.sh
+        13) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board130.git ./ && unzip -u V2B130.zip && rm -rf V2B130.zip && clear && sh init.sh
         ;;
-        14) git clone https://github.com/source235/v2board125.git ./ && unzip -u V2B125.zip && rm -rf V2B125.zip && clear && sh init.sh
+        14) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board125.git ./ && unzip -u V2B125.zip && rm -rf V2B125.zip && clear && sh init.sh
         ;;
-		15) git clone https://github.com/source235/v2board124.git ./ && unzip -u V2B124.zip && rm -rf V2B124.zip && clear && sh init.sh
+	15) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board124.git ./ && unzip -u V2B124.zip && rm -rf V2B124.zip && clear && sh init.sh
         ;;
-        16) git clone https://github.com/source235/v2board123.git ./ && unzip -u V2B123.zip && rm -rf V2B123.zip && clear && sh init.sh
+        16) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board123.git ./ && unzip -u V2B123.zip && rm -rf V2B123.zip && clear && sh init.sh
         ;;
-        17) git clone https://github.com/source235/v2board122.git ./ && unzip -u V2B122.zip && rm -rf V2B122.zip && clear && sh init.sh
+        17) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board122.git ./ && unzip -u V2B122.zip && rm -rf V2B122.zip && clear && sh init.sh
         ;;
-        18) git clone https://github.com/source235/v2board121.git ./ && unzip -u V2B121.zip && rm -rf V2B121.zip && clear && sh init.sh
+        18) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board121.git ./ && unzip -u V2B121.zip && rm -rf V2B121.zip && clear && sh init.sh
         ;;
-        19) git clone https://github.com/source235/v2board120.git ./ && unzip -u V2B120.zip && rm -rf V2B120.zip && clear && sh init.sh
+        19) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board120.git ./ && unzip -u V2B120.zip && rm -rf V2B120.zip && clear && sh init.sh
         ;;
-        20) git clone https://github.com/source235/v2board112.git ./ && unzip -u V2B112.zip && rm -rf V2B112.zip && clear && sh init.sh
+        20) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board112.git ./ && unzip -u V2B112.zip && rm -rf V2B112.zip && clear && sh init.sh
         ;;
-        21) git clone https://github.com/source235/v2board111.git ./ && unzip -u V2B111.zip && rm -rf V2B111.zip && clear && sh init.sh
+        21) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board111.git ./ && unzip -u V2B111.zip && rm -rf V2B111.zip && clear && sh init.sh
         ;;
-        22) git clone https://github.com/source235/v2board110.git ./ && unzip -u V2B110.zip && rm -rf V2B110.zip && clear && sh init.sh
+        22) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board110.git ./ && unzip -u V2B110.zip && rm -rf V2B110.zip && clear && sh init.sh
         ;;
-        23) git clone https://github.com/source235/v2board103.git ./ && unzip -u V2B103.zip && rm -rf V2B103.zip && clear && sh init.sh
+        23) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board103.git ./ && unzip -u V2B103.zip && rm -rf V2B103.zip && clear && sh init.sh
         ;;
-        24) git clone https://github.com/source235/v2board102.git ./ && unzip -u V2B102.zip && rm -rf V2B102.zip && clear && sh init.sh
+        24) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board102.git ./ && unzip -u V2B102.zip && rm -rf V2B102.zip && clear && sh init.sh
         ;;
-        25) git clone https://github.com/source235/v2board101.git ./ && unzip -u V2B101.zip && rm -rf V2B101.zip && clear && sh init.sh
+        25) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board101.git ./ && unzip -u V2B101.zip && rm -rf V2B101.zip && clear && sh init.sh
         ;;
-        26) git clone https://github.com/source235/v2board100.git ./ && unzip -u V2B100.zip && rm -rf V2B100.zip && clear && sh init.sh
+        26) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board100.git ./ && unzip -u V2B100.zip && rm -rf V2B100.zip && clear && sh init.sh
         ;;
-        27) git clone https://github.com/source235/v2board010.git ./ && unzip -u V2B010.zip && rm -rf V2B010.zip && clear && sh init.sh
+        27) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/timeinstallv2board.sh) && git clone https://github.com/source235/v2board010.git ./ && unzip -u V2B010.zip && rm -rf V2B010.zip && clear && sh init.sh
         ;;
-        28) bash <(curl -Ls https://raw.githubusercontent.com/source235/test/main/test1.sh)
+        28) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonsource.sh)
         ;;
         *) echo -e "  Vui Lòng Nhập Số Chính Xác [1-28]$" && exit
         ;;
