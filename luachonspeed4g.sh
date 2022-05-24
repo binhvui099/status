0cab4f98a70a751bd7a3118a2a645eb032a1d0a4
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-24 18:26:45 +0700

    Update luachonspeed4g.sh

--- luachonspeed4g.sh
+++ luachonspeed4g.sh
@@ -30,9 +30,9 @@ echo -e ""
     echo && read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
 
     case "${num}" in
-        1) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/xrayr/main/v2ray/speed4g/install.sh)
+        1) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/Nghi235/config/main/speed4g/v2ray/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/setup.sh) && nano /etc/XrayR/config.yml
         ;;
-        2) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/xrayr/main/trojan/speed4g/install.sh)
+        2) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/Nghi235/config/main/speed4g/trojan/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/setup.sh) && nano /etc/XrayR/config.yml
         ;;
         *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" && bash <(curl -Ls https://github.com/Nghi235/status/blob/main/luachonspeed4g.sh)
         ;;
