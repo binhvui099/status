3055fbf31cf229b0480afb23dc53353cf7962a1b
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-25 17:42:43 +0700

    Update luachonwebxrayrvip.sh

--- luachonwebxrayrvip.sh
+++ luachonwebxrayrvip.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf runxrayrvip.x
 clear
 echo -e "     1. SPEED4G.XYZ"
 echo -e "     2. VPNDATA.XYZ"
@@ -9,6 +10,6 @@ read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
         ;;
         2) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/xrayrvpndata.sh)
         ;;
-        *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" && bash <(curl -Ls https://github.com/Nghi235/status/blob/mailuachonspeed4gn.sh)
+        *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" && bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonwebxrayrvip.sh)
         ;;
     esac
