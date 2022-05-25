a1cc5a7b10b6201ae970be9de1ff5b39f4244bf0
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-25 17:41:23 +0700

    Update luachonwebxrayrvip.sh

--- luachonwebxrayrvip.sh
+++ luachonwebxrayrvip.sh
@@ -1,11 +1,14 @@
-#!/usr/bin/env bash
-echo && read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
+#!/bin/bash
+clear
+echo -e "     1. SPEED4G.XYZ"
+echo -e "     2. VPNDATA.XYZ"
+read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
 
     case "${num}" in
-        1) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/Nghi235/config/main/speed4g/v2ray/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/setup.sh) && nano /etc/XrayR/config.yml
+        1) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/xrayrspeed4g.sh)
         ;;
-        2) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/Nghi235/config/main/speed4g/trojan/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/setup.sh) && nano /etc/XrayR/config.yml
+        2) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/xrayrvpndata.sh)
         ;;
-        *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" && bash <(curl -Ls https://github.com/Nghi235/status/blob/main/luachonspeed4g.sh)
+        *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" && bash <(curl -Ls https://github.com/Nghi235/status/blob/mailuachonspeed4gn.sh)
         ;;
     esac
