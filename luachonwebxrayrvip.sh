7cef7d659c8e92b5b232a5d3ff8421b27e59ce88
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-26 11:39:27 +0700

    Update luachonwebxrayrvip.sh

--- luachonwebxrayrvip.sh
+++ luachonwebxrayrvip.sh
@@ -8,15 +8,15 @@ echo -e "     1. SPEED4G.XYZ"
 echo -e "     2. VPNDATA.XYZ"
 echo -e "     3. EZDIGI.BIZ"
 echo -e ""
-read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
+read -p "  Vui Lòng Nhập Số Để Chọn Website Của Mình: " num
 
     case "${num}" in
-        1) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/xrayrspeed4g.sh)
+        1) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonxrayrvipspeed4g.sh)
         ;;
-        2) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/xrayrvpndata.sh)
+        2) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonxrayrvipvpndata.sh)
         ;;
-        3) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/xrayrvpndata.sh)
+        3) bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonxrayrvipezdigi.sh)
         ;;
-        *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" && bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonwebxrayrvip.sh)
+        *) echo -e "  Chỉ Có Thể Nhập Các Số Đã Thêm" && sleep 3 && bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonwebxrayrvip.sh)
         ;;
     esac
